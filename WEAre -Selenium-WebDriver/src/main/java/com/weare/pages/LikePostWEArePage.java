5e4d7b60a74a4391c8e69caf25420b8f41426c35
IVelchef <ivan.velchev.a61@learn.telerikacademy.com>
2024-11-25 17:19:19 +0200

    Add files via upload

---
+++ WEAre -Selenium-WebDriver/src/main/java/com/weare/pages/LikePostWEArePage.java
@@ -0,0 +1,50 @@
+package com.weare.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LikePostWEArePage extends BaseWEArePage {
+
+
+    private final By likeBtnLocator = By.xpath("//input[@value='Like']");
+    private final By disLikeBtnLocator = By.xpath("//input[@value='Dislike']");
+
+
+    public LikePostWEArePage() {
+        super("/posts#");
+    }
+
+
+    public boolean likePost() {
+
+        WebDriverWait wait = new WebDriverWait(driver(), Duration.ofSeconds(2));
+
+        try {
+            WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(likeBtnLocator));
+
+            if (submitButton.isEnabled()) {
+                submitButton.click();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (TimeoutException e) {
+            return false;
+        }
+
+
+    }
+
+
+    public String getLikeButtonValue() {
+
+        WebDriverWait wait = new WebDriverWait(driver(), Duration.ofSeconds(10));
+        WebElement likeButton = wait.until(ExpectedConditions.visibilityOfElementLocated(disLikeBtnLocator));
+        return likeButton.getAttribute("value");
+    }
+}
---
+++ WEAre -Selenium-WebDriver/src/test/java/wearetests/web/failuretomeetrequirements/WEAreCreateUserBVATest.java
@@ -0,0 +1,64 @@
+package wearetests.web.failuretomeetrequirements;
+
+import com.testData.TestData;
+import com.testData.UniqueNameGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import wearetests.core.WEAreBaseTest;
+
+public class WEAreCreateUserBVATest extends WEAreBaseTest {
+
+
+    @Test
+    public void createUserWithShortPassword() {
+        addIssueAndLink("Jira-141", "Jira Task", "https://a61-qa-team-1.atlassian.net/jira/software/c/projects/IAG/list?selectedIssue=IAG-141");
+
+        String uniqueName = UniqueNameGenerator.getInstance().generateUniqueName();
+
+        registerWEArePage.navigate();
+        registerWEArePage.submitRegisterForm(uniqueName, TestData.getValidEmail(),
+                TestData.getShortPassword(),
+                TestData.getShortConfPassword()
+        );
+
+        String actualPageTitle = registerWEArePage.getPasswordErrorTitle();
+
+        Assertions.assertEquals("password must be minimum 8 characters", actualPageTitle, "Page title is not correct.");
+    }
+
+
+    @Test
+    public void createUserWithLongUsername() {
+        addIssueAndLink("Jira-142", "Jira Task", "https://a61-qa-team-1.atlassian.net/jira/software/c/projects/IAG/list?selectedIssue=IAG-142");
+
+        String uniqueSecondName = UniqueNameGenerator.getInstance().generateUniqueName() + "BillyGibbonsTestUser";
+        registerWEArePage.navigate();
+        registerWEArePage.submitRegisterForm(uniqueSecondName, TestData.getValidEmail(),
+                TestData.getValidPassword(),
+                TestData.getConfValidPassword());
+
+
+        String actualPageTitle = registerWEArePage.getUsernameErrorTitle();
+        Assertions.assertEquals("username must be maximum 20 characters", actualPageTitle, "Page title is not correct.");
+
+    }
+
+
+    @Test
+    public void createUserWithExistingEmail() {
+        addIssueAndLink("Jira-143", "Jira Task", "https://a61-qa-team-1.atlassian.net/jira/software/c/projects/IAG/list?selectedIssue=IAG-143");
+
+        String username = UniqueNameGenerator.getInstance().generateUniqueName();
+        registerWEArePage.navigate();
+        registerWEArePage.submitRegisterForm(username, TestData.getValidEmail(),
+                TestData.getValidPassword(),
+                TestData.getConfValidPassword()
+        );
+
+
+        String actualPageTitle = registerWEArePage.getEmailErrorTitle();
+        Assertions.assertEquals("the email already exist", actualPageTitle, "Page title is not correct.");
+
+    }
+}
+
